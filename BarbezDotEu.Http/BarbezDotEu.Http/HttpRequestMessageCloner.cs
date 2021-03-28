093d3ccec7141f686730a9e1766b0da2a4b86b54
Hannes Barbez <hannesbarbez@hotmail.com>
2021-03-28 20:12:39 +0200

    Added initial logic split off from Thesis project;

---
+++ BarbezDotEu.Http/BarbezDotEu.Http/HttpRequestMessageCloner.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Hannes Barbez. All rights reserved.
+// Licensed under the GNU General Public License v3.0
+
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BarbezDotEu.Http
+{
+    /// <summary>
+    /// Helper class for cloning <see cref="HttpRequestMessage"/>s.
+    /// </summary>
+    public static class HttpRequestMessageCloner
+    {
+        /// <summary>
+        /// Clones a given <see cref="HttpRequestMessage"/>.
+        /// </summary>
+        /// <remarks>
+        /// Adapted from: https://stackoverflow.com/a/46026230
+        /// </remarks>
+        public static async Task<HttpRequestMessage> Clone(HttpRequestMessage request)
+        {
+            var clone = new HttpRequestMessage(request.Method, request.RequestUri)
+            {
+                Content = await CloneHttpContent(request.Content),
+                Version = request.Version
+            };
+
+            foreach (var option in request.Options)
+                clone.Options.TryAdd(option.Key, option.Value);
+
+            foreach (var header in request.Headers)
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+
+            return clone;
+        }
+
+        /// <summary>
+        /// Clones given <see cref="HttpContent"/>.
+        /// </summary>
+        /// <remarks>
+        /// Adapted from: https://stackoverflow.com/a/46026230
+        /// </remarks>
+        private static async Task<HttpContent> CloneHttpContent(HttpContent content)
+        {
+            if (content == null) return null;
+            var memoryStream = new MemoryStream();
+            await content.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            var clone = new StreamContent(memoryStream);
+            foreach (var header in content.Headers)
+                clone.Headers.Add(header.Key, header.Value);
+
+            return clone;
+        }
+    }
+}
