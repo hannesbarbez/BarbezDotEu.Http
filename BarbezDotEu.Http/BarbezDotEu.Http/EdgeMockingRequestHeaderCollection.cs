2d0e5c803f6cafc545a1c0564efb39d76a564e04
Hannes Barbez <hannesbarbez@hotmail.com>
2024-12-12 22:33:33 +0100

    EdgeMockingRequestHeaderCollection

--- BarbezDotEu.Http/BarbezDotEu.Http/EdgeMockingRequestHeaderCollection.cs
+++ BarbezDotEu.Http/BarbezDotEu.Http/EdgeMockingRequestHeaderCollection.cs
@@ -10,14 +10,14 @@ using System.Net.Mime;
 namespace BarbezDotEu.Http
 {
     /// <summary>
-    /// Mocks headers that would've been sent typically by Microsoft Edge during the last half of 2023.
+    /// Mocks headers that would've been sent typically by Microsoft Edge during the last quarter of 2024.
     /// </summary>
     public class EdgeMockingRequestHeaderCollection
     {
         /// <summary>
         /// Gets an Edge style user agent header.
         /// </summary>
-        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0";
+        public const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0";
 
         /// <summary>
         /// Gets Edge style accept headers.
